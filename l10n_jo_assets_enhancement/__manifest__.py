c72c4f9950df3ac33f98899afcc786a66cfc6521
Hammam <hammtara56@gmail.com>
2022-03-05 20:13:19 +0300

    1

---
+++ l10n_jo_assets_enhancement/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'RL - Jordan Assets Enhancements',
+    'version': '15.0.0',
+    'category': 'Localization',
+    'website' : 'www.royalline.gold',
+    'author' : 'Royal Line for Shipping Services',
+    'description': "Localization for company assets based on Jordan standards",
+    'depends': ['account','account_asset'],
+    'data': ['view/view.xml'],
+    'installable': True,
+}
