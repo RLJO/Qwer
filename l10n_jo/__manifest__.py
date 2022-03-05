5909b6eee187f2fa347d6797a922085650237783
Hammam <hammtara56@gmail.com>
2022-03-05 20:42:31 +0300

    3

--- l10n_jo/__manifest__.py
+++ l10n_jo/__manifest__.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2011 Cubic ERP - Teradata SAC. (https://cubicerp.com).
+
 {
-    'name': 'RL - Jordan Chart of Accounting',
-    'version': '15.0.0',
-    'category': 'Localization',
-    'description': """
-This is the base module to manage the accounting chart for Jordan in Odoo.
-==============================================================================
+    "name": "Hammam - Accounting",
+    "version": "2.0",
+    "description": """
+Bolivian accounting chart and tax localization.
+
+Plan contable boliviano e impuestos de acuerdo a disposiciones vigentes
+
     """,
-    'website': 'www.royalline.gold',
-    'author': 'Royal Line for Shipping Services',
-    'depends': ['account','l10n_multilang',],
-    'data': [
+    "author": "Hemo",
+    'category': 'Accounting/Localizations/Account Charts',
+    "depends": ["account"],
+    "data": [
+        "data/account_jo_coa_data.xml",
+        "data/account.account.template.csv",
+        "data/account_tax_template_data.xml",
         'data/account.group.csv',
         'data/account_chart_template_data.xml',
-        'data/account.account.template.csv',
-        'data/account_jo_coa_data.xml',
-        'data/account_data.xml',
-        'data/account_tax_template_data.xml',
-        'data/fiscal_templates_data.xml',
-#         'data/res.lang.csv',
-#         'data/account_chart_template_data.yml',
+        "data/account_data.xml",
+        "data/fiscal_templates_data.xml",
+        "data/res.lang.csv",
+        ],
+    'demo': [
+        'demo/demo_company.xml',
     ],
+    'license': 'LGPL-3',
 }
